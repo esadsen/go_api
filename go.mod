06ac98377243cf3d10325b9521875a0d8c3c69a3
Esad Sen <90692939+esadsen@users.noreply.github.com>
2024-08-25 15:15:55 +0300

    api finished

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/esadsen/go_api
+
+go 1.22.5
+
+require (
+	github.com/avukadin/goapi v0.0.0-20230822123528-a98a49676213 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ internal/middleware/authorization.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/avukadin/goapi/internal/tools"
+	"github.com/esadsen/go_api/api"
+	log "github.com/sirupsen/logrus"
+)
+
+var UnAuthorizedError = errors.New("Invalid username or taken.")
+
+func Authorization(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+		println(err)
+		
+		if username == "" || token == "" {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+
+		var database * tools.DatabaseInterface
+		database, err = tools.NewDatabase()
+		if err != nil{
+			api.InternalErrorHandler(w)
+			return
+		}
+
+		var loginDetails *tools.loginDetails
+		loginDetails = database.Login(username, token)
+		if (loginDetails == nil || (token != (*loginDetails).AuthToken)){
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
